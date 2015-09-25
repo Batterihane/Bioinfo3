05a7f031ca5f04bca2d86f1cb6a98e7c17ea07ff
Lenaq <nikolajskipperrasmussen@gmail.com>
2015-09-25 13:01:06 +0200

    Added FastaParser that can parse multiple sequences in the same file

---
+++ src/solution/helpers/FastaParser.java
@@ -0,0 +1,52 @@
+package solution.helpers;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FastaParser {
+
+    private final File file;
+
+    public FastaParser(String filePathName) {
+        file = new File(filePathName);
+    }
+
+    public List<String> parseFastaFile() throws FileNotFoundException {
+        ArrayList<String> resultList = new ArrayList<>();
+        boolean first = true;
+        String accLine = "";
+
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                if (line.charAt(0) == '>') {
+                    if (first)
+                        first = false;
+                    else
+                    {
+                        resultList.add(accLine);
+                        accLine = "";
+                    }
+                }
+                else {
+                    accLine += line;
+                }
+            }
+        }
+        resultList.add(accLine);
+        return resultList;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        FastaParser fastaParser = new FastaParser("brca1-testseqs.fasta");
+        List<String> resultList = fastaParser.parseFastaFile();
+        System.out.println(resultList.toString());
+
+        System.out.println("size: " + resultList.size());
+
+    }
+
+
+}
